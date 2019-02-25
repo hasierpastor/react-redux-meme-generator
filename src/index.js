6ba75bc75c5968ab9858138d318b8656e727f288
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-25 13:57:21 -0800

    wrapped app in provider

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
