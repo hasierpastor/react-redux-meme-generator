5d54d0dc1846d58d043e42babc8b0bd0e54321b3
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-25 13:49:51 -0800

    added form which adds meme

--- src/Component/MemeForm.js
+++ src/Component/MemeForm.js
@@ -12,6 +12,51 @@ class AddMemeForm extends Component {
   }
 
   handleSubmit = evt => {
-    evt.preventDefaul();
+    evt.preventDefault();
+    this.props.addMeme(this.state);
+    this.setState({
+      image: '',
+      topText: '',
+      bottomText: '',
+      id: uuid()
+    });
   };
+
+  handleChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="image">Meme Url:</label>
+        <input
+          id="image"
+          name="image"
+          value={this.state.image}
+          onChange={this.handleChange}
+        />
+
+        <label htmlFor="topText">Top Text:</label>
+        <input
+          id="topText"
+          name="topText"
+          value={this.state.topText}
+          onChange={this.handleChange}
+        />
+
+        <label htmlFor="bottomText">Bottom Text:</label>
+        <input
+          id="bottomText"
+          name="bottomText"
+          value={this.state.bottomText}
+          onChange={this.handleChange}
+        />
+
+        <button>Add a Meme!</button>
+      </form>
+    );
+  }
 }
+
+export default AddMemeForm;
